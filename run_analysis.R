a3247fed8abcb325e2f237035435df22a99d8109
Anika Petrovic <anika.petrovic1999@gmail.com>
2025-03-28 22:47:10 +0100

    Download and unzip dataset

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,6 @@
+xlibrary(dplyr)
 
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "./dataset.zip")
+
+unzip(zipfile = "./dataset.zip", exdir = ".")
